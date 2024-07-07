71a6317cf6ddfb65b0ccf39d5a8f84aba7b41170
N16H7H4WK-3R <aryan014kumar@gmail.com>
2024-07-07 13:04:46 +0530

    Profile UI

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Footer from "./components/views/footer";
 import ManageBlogs from "./components/BlogContent/ManageBlog";
 import NotFoundPage from "./components/utilities/NotFoundPage";
 import './assets/App.css'
+import Profile from "./components/UserAuth/profile";
 
 function App() {
 
@@ -21,6 +22,7 @@ function App() {
         <Route path="/home" element={<> <Header /> <Body /> <Footer />  </>} />
         <Route path="/new-blog" element={<><Header /> <CreateBlog /> <Footer /> </>} />
         <Route path="/manage-blogs" element={<><Header /> <ManageBlogs /> <Footer /> </>} />
+        <Route path="/user-profile" element={<><Header /> <Profile /> <Footer /> </>} />
         <Route path="*" element={<><Header /> <NotFoundPage /> <Footer /> </>} />
       </Routes>
     </BrowserRouter>
